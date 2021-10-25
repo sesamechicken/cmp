5d2e5b3fc93fd2dc0ecfaf5e1cafd77cf405a267
sesamechicken <chrisdickow@gmail.com>
2021-10-25 16:32:02 -0400

    @sesamechicken | WIP

--- src/components/homegallery/index.js
+++ src/components/homegallery/index.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Masonry from '@mui/lab/Masonry';
 import MasonryItem from '@mui/lab/MasonryItem';
+import { TOKEN } from '../../utils';
+
 import './homegallery.css';
 
 const HomeGallery = () => {
@@ -10,7 +12,7 @@ const HomeGallery = () => {
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
-        'https://cdn.contentful.com/spaces/jr0ojhkrepkp/environments/master/assets?access_token=W2VvuM6-bxUfGHjFWvqNYPazJNKhI3uaiVaSmg_QMPo',
+        `https://cdn.contentful.com/spaces/jr0ojhkrepkp/environments/master/assets?access_token=${TOKEN}`,
       );
 
       setData(result.data.items);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,1 @@
-// import contentful from 'contentful';
-
-// const client = contentful.createClient({
-//   space: 'jr0ojhkrepkp',
-//   accessToken: 'W2VvuM6-bxUfGHjFWvqNYPazJNKhI3uaiVaSmg_QMPo'
-// });
-
-// export const getContent = () => {
-//   client.getEntries().then((entries) => entries);
-// };
+export const TOKEN = process.env.TOKEN || '';
